0379dd30afe98ee539b8770350884ddd451b2fba
YOUR NAMETenit2012 <tenit2012@gmail.com>
2017-03-11 21:38:12 -0800

    comment

---
+++ webbuaxua/src/main/java/com/nlu/cdw/ten/WebbuaxuaApplication.java
@@ -0,0 +1,12 @@
+package com.nlu.cdw.ten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbuaxuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbuaxuaApplication.class, args);
+	}
+}
